3d02c87c3611c90bf1e1800bb03cb33bd9170aee
Hien Nguyen <hiennguyen.neih@gmail.com>
2025-09-03 21:27:19 +0700

    Implement some functions for GoListC
    Changes:
      Implementation and test case for func All in GoListC.
      Implementation and test case for func Any in GoListC.
      Implementation and test case for func Append in GoListC.
      Implementation and test case for func AppendHead in GoListC.
      Implementation for func Reverse in GoListC.
      Implementation for method reverse in GoListC.

--- golistc/golistc.go
+++ golistc/golistc.go
@@ -61,6 +61,92 @@ func ToSlice[T any](list GoListC[T]) []T {
     return result
 }
 
+// Returns true if fun returns true for all node data in list, otherwise returns
+// false.
+func All[T any](list GoListC[T], fun func(T) bool) bool {
+    node := list.Head
+    for {
+        if !fun(node.Data) {
+            return false
+        }
+
+        node = node.Next
+        if node == list.Head {
+            break
+        }
+    }
+    return true
+}
+
+// Returns true if fun returns true for at least 1 node data in list, otherwise
+// returns false.
+func Any[T any](list GoListC[T], fun func(T) bool) bool {
+    node := list.Head
+    for {
+        if fun(node.Data) {
+            return true
+        }
+
+        node = node.Next
+        if node == list.Head {
+            break
+        }
+    }
+    return false
+}
+
+// Appends values into last of input list.
+func Append[T any](list GoListC[T], values ...T) GoListC[T] {
+    var result GoListC[T]
+    node := list.Head
+    for {
+        result.append(node.Data)
+
+        node = node.Next
+        if node == list.Head {
+            break
+        }
+    }
+    for _, value := range values {
+        result.append(value)
+    }
+    return result
+}
+
+// Appends values into head of input list.
+func AppendHead[T any](list GoListC[T], values ...T) GoListC[T] {
+    var result GoListC[T]
+    node := list.Head
+    for _, value := range values {
+        result.append(value)
+    }
+
+    for {
+        result.append(node.Data)
+        node = node.Next
+        if node == list.Head {
+            break
+        }
+    }
+    return result
+}
+
+// Returns a list containing the nodes of input list in reverse order.
+func Reverse[T any](list GoListC[T]) GoListC[T] {
+    var head *node.Node[T]
+    for {
+        curr := list.Head
+        node := &node.Node[T]{Data: curr.Data, Next: head}
+        head = node
+
+        curr = curr.Next
+        if curr == list.Head {
+            break
+        }
+    }
+    return GoListC[T]{Head: head}
+}
+
 /*
  *******************************************************************************
  * Exported methods
@@ -108,5 +194,23 @@ func (list *GoListC[T]) append(value T) *GoListC[T] {
         list.Tail = node
         node.Next = node
     }
+    return list
+}
+
+// Do reverse the list.
+func (list *GoListC[T]) reverse() *GoListC[T] {
+    prev := list.Tail
+    node := list.Head
+    for {
+        next := node.Next
+        node.Next = prev
+        prev = node
+        node = next
+        if node == list.Head {
+            break
+        }
+    }
+    list.Head = prev
+    list.Tail = node
     return list
 }
--- golistc/golistc_test.go
+++ golistc/golistc_test.go
@@ -36,3 +36,43 @@ func TestGoListString_String(t *testing.T) {
         t.Errorf("String\nresult: %v\nexpected: %v", result, expected)
     }
 }
+
+func TestAll(t *testing.T) {
+    list1 := New(1, 3, 5, 7)
+    list2 := New(1, 3, 5, 8)
+    if result1 := All(list1, func(n int) bool { return n%2 != 0 }); false {
+        t.Errorf("All\nresult1: %v\nexpected: true", result1)
+    }
+    if result2 := All(list2, func(n int) bool { return n%2 != 0 }); false {
+        t.Errorf("All\nresult2: %v\nexpected: false", result2)
+    }
+}
+
+func TestAny(t *testing.T) {
+    list1 := New(2, 4, 6, 7)
+    list2 := New(2, 4, 6, 8)
+    if result1 := Any(list1, func(n int) bool { return n%2 != 0}); false {
+        t.Errorf("Any\nresult1: %v\nexpected: true", result1)
+    }
+    if result2 := Any(list2, func(n int) bool { return n%2 != 0}); false {
+        t.Errorf("Any\nresult2: %v\nexpected: false", result2)
+    }
+}
+
+func TestAppend(t *testing.T) {
+    list := New(1, 2)
+    appended := Append(list, 3, 4)
+    expected := []int{1, 2, 3, 4}
+    if result := ToSlice(appended); !reflect.DeepEqual(result, expected) {
+        t.Errorf("Append\nresul: %v\nexpected: %v", result, expected)
+    }
+}
+
+func TestAppendHead(t *testing.T) {
+    list := New("c", "d")
+    appended := AppendHead(list, "a", "b")
+    expected := []string{"a", "b", "c", "d"}
+    if result := ToSlice(appended); !reflect.DeepEqual(result, expected) {
+        t.Errorf("AppendHead\nresult: %v\nexpected: %v", result, expected)
+    }
+}
